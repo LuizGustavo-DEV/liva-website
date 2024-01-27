da7f1e8ec651ef1d652ed8e0504ed3d64d2ee007
LuizGustavo-DEV <luizgustavoaru9994@gmail.com>
2024-01-26 21:23:04 -0300

    Progresso - 26/01/2024 - Noite

--- assets/js/menu.js
+++ assets/js/menu.js
@@ -1,3 +1,17 @@
 $(document).ready(() => {
+    let active = false;
 
+    $('.menu').on('click', function () {
+        if (active) {
+            active = false;
+
+            $('.menu').find('span').removeClass('close-menu').addClass('gg-menu');
+            $('.mobile-menu').removeClass('active');
+        } else {
+            active = true;
+
+            $('.menu').find('span').removeClass('gg-menu').addClass('close-menu');
+            $('.mobile-menu').addClass('active');
+        }
+    });
 });
